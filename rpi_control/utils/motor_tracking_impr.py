2a0878d43b4777c45b5a357615ec261df1f34fda
mntijn <montijn02@gmail.com>
2024-12-02 16:57:49 +0100

    update

--- rpi_control/utils/motor_tracking_impr.py
+++ rpi_control/utils/motor_tracking_impr.py
@@ -52,7 +52,10 @@ class FaceTrackingSystem:
         self.smoothing_factor = 0.5
 
         self.kit = servo_kit
-        
+        self.servo0_angle = 90  # Up/Down servo angle
+        self.servo2_angle = 90  # Left/Right servo angle
+        self.servo_min_angle = 0
+        self.servo_max_angle = 180
 
         # Deadzone parameters
         self.deadzone_x = 50
