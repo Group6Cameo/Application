1c4400bc9ce3184d219b52bb66304347313f9fe5
mntijn <montijn02@gmail.com>
2024-12-03 12:27:16 +0100

    import

--- rpi_control/widgets/face_tracking.py
+++ rpi_control/widgets/face_tracking.py
@@ -1,5 +1,6 @@
-from PyQt6.QtWidgets import QWidget, QVBoxLayout, QPushButton, QLabel, QHBoxLayout, QComboBox, QTimer
-from PyQt6.QtCore import QThread, pyqtSignal, Qt
+from PyQt6.QtWidgets import (QWidget, QVBoxLayout, QPushButton, QLabel,
+                             QHBoxLayout, QComboBox)
+from PyQt6.QtCore import QThread, pyqtSignal, Qt, QTimer
 from PyQt6.QtGui import QImage, QPixmap
 from ..utils.motor_tracking_impr import FaceTrackingSystem
 from adafruit_servokit import ServoKit
