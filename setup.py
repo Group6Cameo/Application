d17a97d054fb7533676520b7338e7c8f8f8c9dc5
mntijn <montijn02@gmail.com>
2024-12-15 15:24:25 +0100

    oops

--- setup.py
+++ setup.py
@@ -22,18 +22,19 @@ setup(
         "picamera2",
         "fastapi",
         "uvicorn[standard]",
-    entry_points = {
+    ],
+    entry_points={
         'console_scripts': [
             'rpi-control=rpi_control.launcher:main',
             'rpi-control-gui=rpi_control.main:main',
             'rpi-control-server=rpi_control.server:main',
         ],
     },
-    author= "Montijn van den Beukel",
-    description= "A Raspberry Pi control interface for Cameo",
-    keywords= "raspberry-pi, qt, wifi",
-    python_requires= ">=3.7",
-    package_data = {
+    author="Montijn van den Beukel",
+    description="A Raspberry Pi control interface for Cameo",
+    keywords="raspberry-pi, qt, wifi",
+    python_requires=">=3.7",
+    package_data={
         'rpi_control': ['assets/*'],
     },
 )
